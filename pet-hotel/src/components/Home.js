c1201b44f713ebcc0761edbf3460ae8c4936d932
Ahmedin-Jamil <jamil.al.amin1100@gmail.com>
2025-03-26 02:22:55 +0800

    Update Home.js

--- pet-hotel/src/components/Home.js
+++ pet-hotel/src/components/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
                   src={baguioLogo} 
                   alt="Baguio Pet Boarding Logo" 
                   className="img-fluid"
-                  style={{ opacity: 0.7 }}
+                  style={{ opacity: 0 }}
                   
                 />
               </div>
